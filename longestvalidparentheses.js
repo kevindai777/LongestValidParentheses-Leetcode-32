bcccd077515a7737a0bc5d7a15b49b77c1749bf5
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-05 22:11:57 -0400

    solutiosn

---
+++ longestvalidparentheses.js
@@ -0,0 +1,34 @@
+//Objective is to find the length of the longest valid parentheses.
+
+let s = ")()())"
+
+
+//O(n) solution that uses a stack to keep track of parentheses coming in and out.
+
+if (s.length == 0) {
+    return 0
+}
+
+//Push in '-1' to update the first 'max'
+let stack = [-1]
+let max = 0
+
+for (let i = 0; i < s.length; i++) {
+    //Push in if it's an opening parentheses
+    if (s[i] == '(') {
+        stack.push(i)
+    //Pop out if it's a closing parentheses
+    } else {
+        stack.pop()
+        //If the stack is empty, push in new current index
+        if (stack.length == 0) {
+            stack.push(i)
+        } else {
+            //i - stack[stack.length - 1] represents the length
+            //of the longest substring
+            max = Math.max(max, i - stack[stack.length - 1])
+        }
+    }
+}
+
+return max
