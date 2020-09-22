b3030460b601809ec06c4e51cfc9e0784cccccc2
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-22 16:26:24 -0400

    added java solution

---
+++ longestvalidparentheses.java
@@ -0,0 +1,25 @@
+//Java Solution
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack();
+        stack.push(-1);
+        int max = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                
+                if (stack.size() == 0) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        
+        return max;
+    }
+}
